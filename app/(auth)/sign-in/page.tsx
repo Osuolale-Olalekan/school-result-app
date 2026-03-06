581696a33b0a80603eca1dfd77f05c518a53b897
Olalekan <osuolaleolalek7@gmail.com>
2026-03-06 22:45:02 +0100

    `Added Suspense component to SignInPage`

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -1,10 +1,15 @@
 import type { Metadata } from "next";
 import SignInForm from "@/components/auth/SignInForm";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Sign In",
 };
 
 export default function SignInPage() {
-  return <SignInForm />;
+  return (
+    <Suspense fallback={null}>
+      <SignInForm />
+    </Suspense>
+  );
 }
